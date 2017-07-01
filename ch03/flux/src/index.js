08092aa150b3f9fbdc54fe62d9d30dcbc6b10fbb
eden90267 <eden90267@gmail.com>
2017-07-01 18:26:42 +0800

    Learning ch03

--- ch03/flux/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<div />, document.getElementById('root'));
-registerServiceWorker();
