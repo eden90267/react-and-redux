c335d1484135a0b5ae5a11b3f478ac316194a532
eden90267 <eden90267@gmail.com>
2017-08-11 08:24:38 +0800

    Learning ch12

--- ch12/express_server2/src/components/Counter/actions.js
+++
@@ -1,9 +0,0 @@
-import * as ActionTypes from './actionTypes';
-
-export const increment = () => ({
-  type: ActionTypes.INCREMENT
-});
-
-export const decrement = () => ({
-  type: ActionTypes.DECREMENT
-});
--- ch12/express_server2/src/components/Counter/index.js
+++
@@ -1,5 +0,0 @@
-import * as actions from './actions';
-import reducer from './reducer';
-import view, {stateKey} from './view';
-
-export {actions, reducer, view, stateKey};
